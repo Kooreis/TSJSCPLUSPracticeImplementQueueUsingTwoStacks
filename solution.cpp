9f19d861080249eb42b6562bd568e994f0292648
Kooreis <kooreis.official@gmail.com>
2022-07-11 19:51:33 -0500

    Define a Queue class with two stacks s1 and s2. Implement the enqueue operation which adds an element to the queue.

--- solution.cpp
+++ solution.cpp
@@ -1,2 +1,18 @@
-#include <iostream>
-#include <stack>
+class Queue {
+    std::stack<int> s1, s2;
+
+public:
+    void enqueue(int x)
+    {
+        while (!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        s1.push(x);
+
+        while (!s2.empty()) {
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
